e7c2318631626fab32f0f9aaf7d7fd14e1aff658
freddymejia <34697066+freddmejia@users.noreply.github.com>
2024-07-20 01:13:48 +0200

    better room

--- database/migrations/2024_05_03_101742_create_notes_table.php
+++ database/migrations/2024_05_03_101742_create_notes_table.php
@@ -22,6 +22,61 @@ class CreateNotesTable extends Migration
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
+
+        /*Schema::create('alert', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_created_id')->nullable();
+            $table->string('title');
+            $table->string('message');
+            $table->timestamps();
+        });
+
+        Schema::create('detail_group_alert', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('alert_id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('patient_id');
+            $table->string('status');
+            $table->foreign('alert_id')->references('id')->on('alert');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('patient_id')->references('id')->on('patients');
+            $table->timestamps();
+        });*/
+
+
+        /*
+        Schema::create('alert', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_created_id')->nullable();
+            $table->string('title');
+            $table->string('message');
+            $table->string('status');
+            $table->timestamps();
+        });
+
+        Schema::create('detail_group_alert', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('alert_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('resource_type')->nullable();//IF, 1
+            $table->string('resource_id')->nullable();//ID RESOURCE, patient_id
+            $table->string('status');
+            $table->foreign('alert_id')->references('id')->on('alert');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+        */
+
+       /*Schema::create('group_alert', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('alert_id');
+            $table->unsignedBigInteger('patient_id');
+            $table->foreign('alert_id')->references('id')->on('alert');
+            $table->foreign('patient_id')->references('id')->on('patients');
+            $table->timestamps();
+        });*/
+
+
     }
 
     /**
